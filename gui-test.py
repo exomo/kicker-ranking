979cd6419a6605ab475bc5ed0ce4e504c531a8e0
Solmath <thommy86@gmail.com>
2017-11-18 00:22:17 +0100

    Rework creation of new game.

--- gui-test.py
+++ gui-test.py
@@ -6,6 +6,7 @@ from tkinter.messagebox import showinfo
 import time
 import donglebert
 import rfid
+import Game
 #import Tkinter as tk     # python 2
 #import tkFont as tkfont  # python 2
 
@@ -13,6 +14,8 @@ LARGE_FONT= ("Verdana", 12)
 NORM_FONT= ("Verdana", 10)
 SMALL_FONT= ("Verdana", 8)
 
+kickerDB = "kicker_scores.db"
+db = donglebert.Database(kickerDB)
 
 class KickerApp(tk.Tk):
 
@@ -35,7 +38,7 @@ class KickerApp(tk.Tk):
         container.pack(side="top", fill="both", expand=True)
 
         self.frames = {}
-        for F in (GamePage, NewGamePage, PlayerPage, NewPlayerPage, AdminPage):
+        for F in (GamePage, NewGamePage, PlayerPage, AdminPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
 
@@ -55,6 +58,7 @@ class KickerApp(tk.Tk):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+        frame.event_generate("<<ShowFrame>>")
 
 
 class ButtonFrame(tk.Frame):
@@ -104,84 +108,90 @@ class GamePage(tk.Frame):
         spiel4.pack(side="top", fill="x", pady=10)
 
         button1 = tk.Button(self, text="Neues Spiel",
-                            command=lambda: newGame())
+                            command=lambda: self.newGame())
         button1.pack(side="left", fill="both", expand=True)
 
-        def newGame():
-            # spiel-objekt anlegen
-            controller.show_frame("NewGamePage")
-            
-            kickerDB = "kicker_scores.db"
+    def newGame(self):
+        self.Players = []
+        self.numPlayers = 1
 
-            db = donglebert.Database(kickerDB)
 
-            IDpopup("Bitte Token von Spieler 1 scannen.")
-            db.get_player(id)
-        
-            # read first token
-            # spiel.spieler1 = getPlayer(getTokenID())
-            # win.destroy
-            # controller.numPlayers++
-        
-            IDpopup("Bitte Token von Spieler 2 scannen.")
-            id2 = getID()
+        self.win = tk.Toplevel()
+        self.win.wm_title("Window")
 
-            # read second token
-            # spiel.spieler2 = getPlayer(getTokenID())
-            # check for duplicates
-            # win.destroy
-            # controller.numPlayers++
-        
-            IDpopup("Bitte Token von Spieler 3 scannen.")
-            id3 = getID()
+        self.popupMsg = tk.StringVar()
+        self.popupMsg.set("Bitte Token von Spieler %d scannen." % (self.numPlayers))
 
-            # read third token
-            # spiel.spieler3 = getPlayer(getTokenID())
-            # check for duplicates
-            # win.destroy
-            # controller.numPlayers++
-        
-            IDpopup("Bitte Token von Spieler 4 scannen.")
-            id4 = getID()
+        self.l = tk.Label(self.win, textvariable = self.popupMsg)
+        self.l.grid(row=0, column=0)
 
-            # read fourth token
-            # spiel.spieler4 = getPlayer(getTokenID())
-            # check for duplicates
-            # win.destroy
-            # controller.numPlayers++
+        self.after(100, self.SkanToken)
 
-        def IDpopup(msg):
-            popup = tk.Tk()
-            popup.wm_title("!")
-            label = tk.Label(popup, text=msg, font=NORM_FONT)
-            label.pack(side="top", fill="x", pady=10)
-            B1 = tk.Button(popup, text="Okay", command = popup.destroy)
-            B1.pack()
-            popup.mainloop()
+        b = tk.Button(self.win, text="Abbrechen", command=self.win.destroy)
+        b.grid(row=1, column=0)
 
-        def getID():
-            time.sleep(5)
-            return 12345
+    def SkanToken(self):
+        reader = rfid.rfid()
+        tokenID = reader.TryGetToken()
+        if(tokenID != None):
+            self.Players.append(db.get_player(tokenID))
+            self.numPlayers += 1
+            if len(self.Players) == 4:
+                # spiel-objekt anlegen
+                self.win.destroy()
+                spiel = Game.Game()
+                spiel.player1 = self.Players[0]
+                spiel.player2 = self.Players[1]
+                spiel.player3 = self.Players[2]
+                spiel.player4 = self.Players[3]
+                NewGamePage.game = spiel
+                self.controller.show_frame("NewGamePage")
+            else:
+                self.popupMsg.set("Bitte Token von Spieler %d scannen" % (self.numPlayers))
+                self.controller.update_idletasks()
+                self.after(2000, self.SkanToken)                
+        else:
+            self.after(100, self.SkanToken)
 
 
 class NewGamePage(tk.Frame):
+
+    game = None
+    
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.bind("<<ShowFrame>>", self.onShowFrame)
+
         label = tk.Label(self, text="Neues Spiel:", font="Helvetica 12")
-        label.pack(side="top", fill="x", pady=10)
+        #label.pack(side="top", fill="x", pady=10)
+        label.grid(row=0, column=0)
 
-        # wenn frame in vordergrund kommt, abfrage nach token beginnen
-        print("Scan first token")
-        # player1 = getPlayer(getTokenID)
-        # player2 = getPlayer(getTokenID)
-        # player3 = getPlayer(getTokenID)
-        # player4 = getPlayer(getTokenID)
+        self.player1Name = tk.StringVar()
+        self.player1Name.set("Player 1")
+        labelPlayer1 = tk.Label(self, textvariable = self.player1Name)
+        labelPlayer1.grid(row=1, column=0)
 
-        button1 = tk.Button(self, text="Neues Spiel",
-                            command=lambda: controller.show_frame("NewGamePage"))
-        button1.pack(side="left", fill="both", expand=True)
+        self.player2Name = tk.StringVar()
+        self.player2Name.set("Player 2")
+        labelPlayer2 = tk.Label(self, textvariable = self.player2Name)
+        labelPlayer2.grid(row=2, column=0)
 
+        self.player3Name = tk.StringVar()
+        self.player3Name.set("Player 3")
+        labelPlayer3 = tk.Label(self, textvariable = self.player3Name)
+        labelPlayer3.grid(row=1, column=1)
+
+        self.player4Name = tk.StringVar()
+        self.player4Name.set("Player 4")
+        labelPlayer4 = tk.Label(self, textvariable = self.player4Name)
+        labelPlayer4.grid(row=2, column=1)
+
+        button1 = tk.Button(self, text="Sieg Team 1")
+        button1.grid(row=3, column=0)
+
+        button2 = tk.Button(self, text="Sieg Team 2")
+        button2.grid(row=3, column=1)
 
 class PlayerPage(tk.Frame):
  
@@ -192,6 +202,7 @@ class PlayerPage(tk.Frame):
             label.pack(side="top", fill="x", pady=10)
             button = tk.Button(self, text="Neuer Spieler",command=self.popup_bonus)
             button.pack()
+
         def popup_bonus(self):
             self.win = tk.Toplevel()
             self.win.wm_title("Window")
@@ -222,25 +233,11 @@ class PlayerPage(tk.Frame):
             self.e2.grid(row=1, column=1)
             b = tk.Button(self.win, text="Okay", command=self.Safe2DataBase)
             b.grid(row=2, column=0)
+
         def Safe2DataBase(self):
             print("ID: %s\nLast Name: %s" % (self.token, self.e2.get()))
-            kickerDB = "kicker_scores.db"
-            db = donglebert.Database(kickerDB)
             db.add_new_player(self.e2.get(), self.token)
             self.win.destroy()
-
-        
-
-class NewPlayerPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Neuer Spieler", font="Helvetica 12")
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("GamePage"))
-        button.pack()
 
 
 class AdminPage(tk.Frame):
